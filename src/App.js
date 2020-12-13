6623a1d09a35f467f2b6e3b76ab0ee5a3184d281
You Song Hou <yousonghou@yahoo.com>
2020-12-13 00:29:49 -0500

    install react-router-dom and set up router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './App.css';
+import { BrowserRouter as Router, Route  } from 'react-router-dom';
 
+import './App.css';
+import Home from './components/Home';
 import Tasks from './components/tasks/Tasks';
 
 function App() {
   return (
-    <div className="App">
-      <Tasks />
-    </div>
+    <Router className="App">
+      <Route exact path="/" component={Home} />
+      <Route exact path="/tasks" component={Tasks} />
+    </Router>
   );
 }
 
