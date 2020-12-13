0ca136f6ef0b3c2a06cbc7c57ac5634e3bca9350
You Song Hou <yousonghou@yahoo.com>
2020-12-12 22:36:07 -0500

    add a function to create a task

--- contracts/DeMyLogo.sol
+++ contracts/DeMyLogo.sol
@@ -14,4 +14,21 @@ contract DeMyLogo {
     bool completed;
     address payable owner;
   }
+
+  event TaskCreated (
+    uint taskId,
+    string name,
+    string description,
+    string contact,
+    uint amount,
+    bool completed,
+    address payable owner
+  );
+
+  function createTask(string memory _name, string memory _description, string memory _contact, uint _amount) public {
+    taskCount++;
+
+    tasks[taskCount] = Task(taskCount, _name, _description, _contact, _amount, false, msg.sender);
+    emit TaskCreated(taskCount, _name, _description, _contact, _amount, false, msg.sender);
+  }
 }
--- test/DeMyLogo.js
+++ test/DeMyLogo.js
@@ -2,7 +2,7 @@ const { assert } = require('chai');
 
 const DeMyLogo = artifacts.require('./DeMyLogo.sol');
 
-contract(DeMyLogo, (account) => {
+contract(DeMyLogo, ([deployer, user1]) => {
     let deMyLogo;
 
     before(async() => {
@@ -20,6 +20,47 @@ contract(DeMyLogo, (account) => {
         it('has correct name', async() => {
             const name = await deMyLogo.name();
             assert.equal(name, "DeMyLogo");
+        });
+    });
+
+    describe('task', async() => {
+        let result;
+        let taskCount;
+
+        const taskName = "Some Company";
+        const taskDescription = "I need a logo with a cool icon and blue text";
+        const taskContact = "You can contact me at somecompany123@mail.com";
+        const taskAmount = 10;
+
+        before(async() => {
+            result = await deMyLogo.createTask(taskName, taskDescription, taskContact, taskAmount, { from: user1 });
+            taskCount = await deMyLogo.taskCount();
+        });
+
+        it('create task', async() => {
+            const event = result.logs[0].args;
+            assert.equal(event.taskId.toNumber(), taskCount.toNumber(), 'Id is correct');
+            assert.equal(event.name, taskName, 'Name is correct');
+            assert.equal(event.description, taskDescription, 'Description is correct');
+            assert.equal(event.contact, taskContact, 'Contact is correct');
+            assert.equal(event.amount, taskAmount, 'Amount is correct');
+            assert.equal(event.completed, false, 'Completed is correct');
+            assert.equal(event.owner, user1, 'Owner is correct');
+        });
+
+        it('has correct task count', async() => {
+            assert.equal(taskCount, 1);
+        });
+
+        it('lists the task', async() => {
+            const task = await deMyLogo.tasks(taskCount);
+            assert.equal(task.taskId.toNumber(), taskCount.toNumber(), 'Id is correct');
+            assert.equal(task.name, taskName, 'Name is correct');
+            assert.equal(task.description, taskDescription, 'Description is correct');
+            assert.equal(task.contact, taskContact, 'Contact is correct');
+            assert.equal(task.amount, taskAmount, 'Amount is correct');
+            assert.equal(task.completed, false, 'Completed is correct');
+            assert.equal(task.owner, user1, 'Owner is correct');
         });
     });
 })
