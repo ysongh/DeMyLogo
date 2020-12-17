a48ed435ee262970bf0ec5ffce1cbd19453535de
You Song Hou <yousonghou@yahoo.com>
2020-12-16 22:49:41 -0500

    install identicon.js and add an icon on the navbar

--- src/components/tasks/Navbar.js
+++ src/components/tasks/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { AppBar, Container, Toolbar, Link, Typography } from '@material-ui/core';
+import { AppBar, Container, Toolbar, Link, Box, Typography } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
+import Identicon from 'identicon.js';
 
 import Logo from '../../images/logo.png';
 
@@ -12,9 +13,25 @@ const Navbar = ({ account }) => {
           <Link component={RouterLink} to="/">
             <img src={Logo} className="logo" alt="Logo" />
           </Link>
-          <Typography variant="body2">
-            { account }
-          </Typography>
+          
+          <Box display="flex" alignItems="center">
+            <Typography variant="body2">
+              <a
+                target="_blank"
+                className="white-link"
+                rel="noopener noreferrer"
+                href={"https://etherscan.io/address/" + account}>
+                {account.substring(0,10)}...{account.substring(32,42)}
+              </a>
+            </Typography>
+            { account
+              && <img
+                width='30'
+                height='30'
+                src={`data:image/png;base64,${new Identicon(account, 30).toString()}`}
+                alt="Icon" />
+            }
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
